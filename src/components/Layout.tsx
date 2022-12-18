352e7e3d4c5df43500ec047d9032e7da6ebd91e3
swimmingone <dev.swimmingone@gmail.com>
2022-12-18 21:55:08 +0900

    feat: 디어 파비콘 추가

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -10,6 +10,7 @@ const Layout = ({ children }: Props) => {
 		<>
 			<Head>
 				<title>Deer Board</title>
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
 			</Head>
 			<div
 				className={
