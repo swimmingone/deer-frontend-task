5d240ca8bda3e9c6a42daefb63daf2ebbbc33891
swimmingone <dev.swimmingone@gmail.com>
2022-12-18 22:31:14 +0900

    feat: flowbite에서 daisyui로 ui 라이브러리 교체

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./node_modules/flowbite-react/**/*.js",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("flowbite/plugin")],
+	content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/**/components/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [require('daisyui')],
 };
