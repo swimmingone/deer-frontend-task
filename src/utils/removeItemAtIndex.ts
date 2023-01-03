52db4a0803b94e31b8980587f70c7f2bf3542fc7
swimmingone <dev.swimmingone@gmail.com>
2023-01-04 01:36:14 +0900

    chore: 불필요한 provider 와 utils 삭제

--- src/utils/removeItemAtIndex.ts
+++
@@ -1,3 +0,0 @@
-export default function removeItemAtIndex<T>(arr: T[], index: number) {
-	return [...arr.slice(0, index), ...arr.slice(index + 1)];
-}
--- src/utils/replaceItemAtIndex.ts
+++
@@ -1,3 +0,0 @@
-export default function replaceItemAtIndex<T>(arr: T[], index: number, newValue: T) {
-	return [...arr.slice(0, index), newValue, ...arr.slice(index + 1)];
-}
